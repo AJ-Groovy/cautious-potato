c307de442e1ec626e733669a56848334fa877b5e
AJ <yaroslav01593@gmail.com>
2020-01-13 00:06:09 +0200

    removed unnecessary file

--- task1.2.php
+++ task1.2.php
@@ -8,41 +8,41 @@ $age = 80;
 $base_message = 'Ваш возраст ';
 
 switch ($age) {
-    case $age < AGE_YOUNG: {
-            echo ($base_message . 'меньше чем ' . AGE_YOUNG);
-            break;
-        }
-    case $age === AGE_YOUNG: {
-            echo ($base_message . 'равен ' . AGE_YOUNG);
-            break;
-        }
-    case $age > AGE_YOUNG && $age < AGE_MEDIUM_ADULT: {
-            echo ($base_message . 'больше чем ' . AGE_YOUNG . ' но меньше чем ' . AGE_MEDIUM_ADULT);
-            break;
-        }
-    case $age === AGE_MEDIUM_ADULT: {
-            echo ($base_message . 'равен ' . AGE_MEDIUM_ADULT);
-            break;
-        }
-    case $age > AGE_MEDIUM_ADULT && $age < AGE_ADULT: {
-            echo ($base_message . 'больше чем ' . AGE_MEDIUM_ADULT . ' но меньше чем ' . AGE_ADULT);
-            break;
-        }
-    case $age === AGE_ADULT: {
-            echo ($base_message . 'равен ' . AGE_ADULT);
-            break;
-        }
-    case $age > AGE_ADULT && $age < AGE_OLD: {
-            echo ($base_message . 'больше чем ' . AGE_ADULT . ' но меньше чем ' . AGE_OLD);
-            break;
-        }
-    case $age === AGE_OLD: {
-            echo ($base_message . 'равен ' . AGE_OLD);
-            break;
-        }
+  case $age < AGE_YOUNG: {
+      echo ($base_message . 'меньше чем ' . AGE_YOUNG);
+      break;
+    }
+  case $age === AGE_YOUNG: {
+      echo ($base_message . 'равен ' . AGE_YOUNG);
+      break;
+    }
+  case $age > AGE_YOUNG && $age < AGE_MEDIUM_ADULT: {
+      echo ($base_message . 'больше чем ' . AGE_YOUNG . ' но меньше чем ' . AGE_MEDIUM_ADULT);
+      break;
+    }
+  case $age === AGE_MEDIUM_ADULT: {
+      echo ($base_message . 'равен ' . AGE_MEDIUM_ADULT);
+      break;
+    }
+  case $age > AGE_MEDIUM_ADULT && $age < AGE_ADULT: {
+      echo ($base_message . 'больше чем ' . AGE_MEDIUM_ADULT . ' но меньше чем ' . AGE_ADULT);
+      break;
+    }
+  case $age === AGE_ADULT: {
+      echo ($base_message . 'равен ' . AGE_ADULT);
+      break;
+    }
+  case $age > AGE_ADULT && $age < AGE_OLD: {
+      echo ($base_message . 'больше чем ' . AGE_ADULT . ' но меньше чем ' . AGE_OLD);
+      break;
+    }
+  case $age === AGE_OLD: {
+      echo ($base_message . 'равен ' . AGE_OLD);
+      break;
+    }
 
 
-    default: {
-            echo ($base_message . 'больше чем ' . AGE_OLD);
-        }
+  default: {
+      echo ($base_message . 'больше чем ' . AGE_OLD);
+    }
 }
