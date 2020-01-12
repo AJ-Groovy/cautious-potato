d7917534ac50a809613da8de321b1a417b7107a9
AJ <yaroslav01593@gmail.com>
2020-01-12 18:05:02 +0200

    task 1.2 done

---
+++ task1.2.php
@@ -0,0 +1,30 @@
+<?php
+define('AGE_YOUNG', 20);
+define('AGE_MEDIUM_ADULT', 40);
+define('AGE_ADULT', 60);
+define('AGE_OLD', 80);
+
+$age = 76;
+
+switch ($age) {
+    case $age <= AGE_YOUNG: {
+            echo ($base_message . 'меньше чем ' . AGE_YOUNG);
+            break;
+        }
+    case $age > AGE_YOUNG && $age <= AGE_MEDIUM_ADULT: {
+            echo ($base_message . 'больше чем ' . AGE_YOUNG . ' но меньше чем ' . AGE_MEDIUM_ADULT);
+            break;
+        }
+    case $age > AGE_MEDIUM_ADULT && $age <= AGE_ADULT: {
+            echo ($base_message . 'больше чем ' . AGE_MEDIUM_ADULT . ' но меньше чем ' . AGE_ADULT);
+            break;
+        }
+    case $age > AGE_ADULT && $age <= AGE_OLD: {
+            echo ($base_message . 'больше чем ' . AGE_ADULT . ' но меньше чем ' . AGE_OLD);
+            break;
+        }
+
+    default: {
+            echo ($base_message . 'больше чем ' . AGE_OLD);
+        }
+}
