a25131481dec4b9316c857e3b9d39bab56b9b280
AJ <yaroslav01593@gmail.com>
2020-01-17 20:59:12 +0200

    formatting

--- lesson5/arrayMinMax.php
+++ lesson5/arrayMinMax.php
@@ -1,17 +1,18 @@
 <?php
 
-function get_max_value($array) {
+function get_max_value($array)
+{
     return max($array);
 }
 
 
-function get_min_value($array) {
+function get_min_value($array)
+{
     return min($array);
 }
 
 $numbers = [2, -2, 5, 0, -88, 100, -95, 50, 33];
 
-echo(get_max_value($numbers));
-echo('<br>');
-echo(get_min_value($numbers));
-
+echo (get_max_value($numbers));
+echo ('<br>');
+echo (get_min_value($numbers));
--- lesson5/reverseNumber.php
+++ lesson5/reverseNumber.php
@@ -1,8 +1,9 @@
 <?php
 
-function reverse_number($number) {
+function reverse_number($number)
+{
     $array_of_letters = str_split(strval($number));
     return join(array_reverse($array_of_letters));
 }
 
-echo(reverse_number(1000));
+echo (reverse_number(1000));
